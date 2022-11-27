dd2289d7bdac0e412ecd2ac4e347ec2c2f300f8d
Kevin Yong <Kevinyong1023@gmail.com>
2022-11-27 13:42:40 +0800

    Refactor the files and folders and created a practice folder with one practise case.

---
+++ Tutorial/ImportDirectory/functions.py
@@ -0,0 +1,7 @@
+# In this file, we defined all the functions that we are going to use:
+import random
+
+def printOutText(): print("HELLO WORLD")
+
+
+def genRandomNum(): print('{:.2f}'.format(random.random()))
---
+++ Tutorial/ImportDirectory/prints.py
@@ -0,0 +1,7 @@
+# As we need to use the functions that belongs to another directory,
+# we have to import the directory and declare the functions that we need/want to use
+import functions
+from functions import printOutText, genRandomNum
+# First: Directory, Functions that we need
+printOutText()
+genRandomNum()
